892ea61d613c9c4f1646089b3d6e043b219e3a05
yoshiki_murakami <eiim.yoshiki@gmail.com>
2023-08-24 00:25:03 +0900

    add: new view, create action

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'webrick'
 require 'erb'
+require './models/memo'
 
 server = WEBrick::HTTPServer.new(Port: 3000)
 
@@ -20,6 +21,23 @@ server.mount_proc '/' do |req, res|
     html = template.result(binding)
     res.body = html
     # ... 他のHTTPメソッドの処理 ...
+  else
+    # type code here
+  end
+end
+
+server.mount_proc '/new' do |req, res|
+  template = ERB.new(File.read('views/new.html.erb'))
+
+  case req.request_method
+  when 'GET'
+    # ERBテンプレートを評価してHTMLを生成
+    html = template.result(binding)
+    res.body = html
+  when 'POST'
+    Memo.create(name: req.query['name'], content: req.query['content'])
+    # メモ一覧ページにリダイレクト
+    res.set_redirect(WEBrick::HTTPStatus::Found, '/')
   else
     # type code here
   end
--- models/memo.rb
+++ models/memo.rb
@@ -1,7 +1,25 @@
+require 'pg'
+
 class Memo
   attr_accessor :name, :content
+
   def initialize(name, content)
     @name = name
     @content = content
+    puts "hello"
+  end
+
+  class << self
+    def create(name:, content:)
+      # データベースに接続
+      conn = PG.connect(dbname: 'postgres', user: 'postgres', password: 'example', host: 'db')
+
+      # memo テーブルにレコードを追加するSQLコード
+      insert_sql = "INSERT INTO memo (name, content) VALUES ($1, $2)"
+      conn.exec_params(insert_sql, [name, content])
+
+      # データベース接続を閉じる
+      conn.close
+    end
   end
 end
