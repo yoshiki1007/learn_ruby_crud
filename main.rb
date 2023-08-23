11adc98425da0fb55ee0d7d3b2bd23bea0e72226
yoshiki_murakami <eiim.yoshiki@gmail.com>
2023-08-24 00:38:27 +0900

    change: refactor code.

--- main.rb
+++ main.rb
@@ -5,19 +5,16 @@ require './models/memo'
 server = WEBrick::HTTPServer.new(Port: 3000)
 
 server.mount_proc '/' do |req, res|
-  # メモ一覧を格納する配列（仮のデータ）
   memos = Memo.all
 
   template = ERB.new(File.read('views/index.html.erb'))
 
   case req.request_method
   when 'GET'
-    # ERBテンプレートを評価してHTMLを生成
     html = template.result(binding)
     res.body = html
-    # ... 他のHTTPメソッドの処理 ...
   else
-    # type code here
+    res.set_redirect(WEBrick::HTTPStatus::Found, '/')
   end
 end
 
@@ -26,15 +23,13 @@ server.mount_proc '/new' do |req, res|
 
   case req.request_method
   when 'GET'
-    # ERBテンプレートを評価してHTMLを生成
     html = template.result(binding)
     res.body = html
   when 'POST'
     Memo.create(name: req.query['name'], content: req.query['content'])
-    # メモ一覧ページにリダイレクト
     res.set_redirect(WEBrick::HTTPStatus::Found, '/')
   else
-    # type code here
+    res.set_redirect(WEBrick::HTTPStatus::Found, '/')
   end
 end
 
--- models/memo.rb
+++ models/memo.rb
@@ -6,7 +6,6 @@ class Memo
   def initialize(name, content)
     @name = name
     @content = content
-    puts "hello"
   end
 
   DB_NAME = 'postgres'
@@ -16,21 +15,16 @@ class Memo
 
   class << self
     def all
-      # データベースに接続
       conn = PG.connect(dbname: DB_NAME, user: DB_USER, password: DB_PASS, host: DB_HOST)
 
-      # memo テーブルからデータを取得するSQLコード
       select_memos_sql = <<~SQL
         SELECT * FROM memo;
       SQL
 
-      # データベースにクエリを実行し、結果を取得
       result = conn.exec(select_memos_sql)
 
-      # データベース接続を閉じる
       conn.close
 
-      # 結果を配列に変換
       result.map do |row|
         {
           id: row['id'],
@@ -41,14 +35,11 @@ class Memo
     end
 
     def create(name:, content:)
-      # データベースに接続
       conn = PG.connect(dbname: DB_NAME, user: DB_USER, password: DB_PASS, host: DB_HOST)
 
-      # memo テーブルにレコードを追加するSQLコード
       insert_sql = "INSERT INTO memo (name, content) VALUES ($1, $2)"
       conn.exec_params(insert_sql, [name, content])
 
-      # データベース接続を閉じる
       conn.close
     end
   end
